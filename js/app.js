621922f6f6a290f0b558fa014204be5236f5248d
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-02-17 15:11:26 +0600

    erro

--- js/app.js
+++ js/app.js
@@ -21,12 +21,14 @@ document.getElementById('calculate').addEventListener('click',function(){
     getTextValue('erro1').style.display='none';
     getTextValue('erro2').style.display='none';
     getTextValue('erro3').style.display='none';
+    getTextValue('erro4').style.display='none';
+    // erro positive number 
    }else if(income <0 || food<0 ||rent< 0 ||clothes < 0){
-       
     getTextValue('erro').style.display='none';
     getTextValue('erro1').style.display='block';
     getTextValue('erro2').style.display='none';
     getTextValue('erro3').style.display='none';
+    getTextValue('erro4').style.display='none';
    }
    else{
     getTextValue('erro').style.display='none';
@@ -57,11 +59,18 @@ document.getElementById('save-btn').addEventListener('click',function(){
    const saving = income *(save/100);
    const remainingAmount =balance-saving;
     // erro 
-    if (saving > balance) {
+    if(save <0){
     getTextValue('erro3').style.display='block';
+    getTextValue('erro4').style.display='none';
+
+    }
+   else if (saving > balance) {
+    getTextValue('erro4').style.display='block';
+    getTextValue('erro3').style.display='none';
 
 }else{
     getTextValue('erro3').style.display='none';
+    getTextValue('erro4').style.display='none';
 
    getTextValue('saving-amount').innerText=saving;
    getTextValue('remaining-amount').innerText=remainingAmount;
